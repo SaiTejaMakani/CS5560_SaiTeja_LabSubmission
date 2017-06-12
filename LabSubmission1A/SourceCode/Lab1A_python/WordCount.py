77123a321d5b64092460e407b34aa227e02414a4
Sai Teja Makani <sm47d@mail.umkc.edu>
2017-06-12 15:34:18 -0500

    lab1A report documents

---
+++ LabSubmission1A/SourceCode/Lab1A_python/WordCount.py
@@ -0,0 +1,11 @@
+import sys
+file = open("G:\KDM\README.txt", "r+")
+wordcount = {}
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+for key in wordcount.keys():
+  print ("%s %s " % (key+"->", wordcount[key]))
+file.close();
