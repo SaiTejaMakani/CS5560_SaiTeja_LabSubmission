77e5ed3469992f145ac6509807b65fcf5c8bf701
Sai Teja Makani <sm47d@mail.umkc.edu>
2017-07-05 20:24:36 -0500

    lab 4 source code

---
+++ LabSubmission4/SourceCode/SparkOpenIE_WordNET_LDA/src/main/scala/wordnet/WordNetSpark.scala
@@ -0,0 +1,32 @@
+package wordnet
+
+import org.apache.spark.{SparkConf, SparkContext}
+import rita.RiWordNet
+
+/**
+  * Created by Mayanka on 26-06-2017.
+  */
+object WordNetSpark {
+  def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "E:\\winutils")
+    val conf = new SparkConf().setAppName("WordNetSpark").setMaster("local[*]").set("spark.driver.memory", "4g").set("spark.executor.memory", "4g")
+    val sc = new SparkContext(conf)
+
+
+    val data=sc.textFile("data/sentenceSample")
+
+    val dd=data.map(f=>{
+      val wordnet = new RiWordNet("E:\\KDM\\WordNet-3.0")
+      getSynoymns(wordnet,"dog")
+    })
+    dd.take(1).foreach(f=>println(f.mkString(" ")))
+  }
+  def getSynoymns(wordnet:RiWordNet,word:String): Array[String] ={
+    println(word)
+    val pos=wordnet.getPos(word)
+    println(pos.mkString(" "))
+    val syn=wordnet.getAllSynonyms(word, pos(0), 10)
+    syn
+  }
+
+}
